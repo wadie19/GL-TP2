634dd3e9b147bdaae3520743434cfedc3f0d18c9
wadie19 <rezgui.m.w40@gmail.com>
2023-10-27 16:14:01 +0200

    tests ajoutés

--- src/test/java/theatricalplays/StatementPrinterTests.java
+++ src/test/java/theatricalplays/StatementPrinterTests.java
@@ -3,10 +3,12 @@ package theatricalplays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.approvaltests.Approvals.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatementPrinterTests {
 
@@ -40,7 +42,43 @@ public class StatementPrinterTests {
 
         StatementPrinter statementPrinter = new StatementPrinter();
         Assertions.assertThrows(Error.class, () -> {
-            statementPrinter.print(invoice, plays);
+        statementPrinter.print(invoice, plays);
         });
+    }
+
+    @Test
+    void testAudience30ForTragedy() {
+    HashMap<String, Play> plays = new HashMap<>();
+    plays.put("playID", new Play("Play", "tragedy"));
+
+    Invoice invoice = new Invoice("Customer", List.of(new Performance("playID", 30)));
+
+    StatementPrinter statementPrinter = new StatementPrinter();
+    var result = statementPrinter.print(invoice, plays);
+
+    String expectedStatement = "Statement for Customer\n" +
+            "  Play: $400.00 (30 seats)\n" +
+            "Amount owed is $400.00\n" +
+            "You earned 0 credits\n";
+
+    assertEquals(expectedStatement, result);
+    }
+
+    @Test
+    void testAudience20ForComedy() {
+    HashMap<String, Play> plays = new HashMap<>();
+    plays.put("playID", new Play("Play", "comedy"));
+
+    Invoice invoice = new Invoice("Customer", List.of(new Performance("playID", 20)));
+
+    StatementPrinter statementPrinter = new StatementPrinter();
+    var result = statementPrinter.print(invoice, plays);
+
+    String expectedStatement = "Statement for Customer\n" +
+            "  Play: $360.00 (20 seats)\n" +
+            "Amount owed is $360.00\n" +
+            "You earned 4 credits\n"; 
+
+    assertEquals(expectedStatement, result);
     }
 }
